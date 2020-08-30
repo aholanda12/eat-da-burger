9f73f4e5d93e67699d59d4fbf635ea3ba2320db0
Ashley Holanda <aholanda12@gmail.com>
2020-08-29 21:51:55 -0400

    last minute changes before heroku deployment

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Cheesburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Chicken Sandwich', false);
